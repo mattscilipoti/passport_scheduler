cbed55fabecf17230c08e4a15a4b9670dd989318
mattscilipoti <matt.scilipoti@ga.co>
2016-05-13 15:53:57 -0400

    Update UserDashboard using generator

--- app/dashboards/user_dashboard.rb
+++ app/dashboards/user_dashboard.rb
@@ -8,6 +8,7 @@ class UserDashboard < Administrate::BaseDashboard
   # which determines how the attribute is displayed
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
+    invited_by: Field::Polymorphic,
     id: Field::Number,
     email: Field::String,
     encrypted_password: Field::String,
@@ -26,6 +27,13 @@ class UserDashboard < Administrate::BaseDashboard
     confirmed_at: Field::DateTime,
     confirmation_sent_at: Field::DateTime,
     unconfirmed_email: Field::String,
+    role: Field::String.with_options(searchable: false),
+    invitation_token: Field::String,
+    invitation_created_at: Field::DateTime,
+    invitation_sent_at: Field::DateTime,
+    invitation_accepted_at: Field::DateTime,
+    invitation_limit: Field::Number,
+    invitations_count: Field::Number,
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -34,15 +42,16 @@ class UserDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
+    :invited_by,
     :id,
     :email,
     :encrypted_password,
-    :reset_password_token,
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = [
+    :invited_by,
     :id,
     :email,
     :encrypted_password,
@@ -61,12 +70,20 @@ class UserDashboard < Administrate::BaseDashboard
     :confirmed_at,
     :confirmation_sent_at,
     :unconfirmed_email,
+    :role,
+    :invitation_token,
+    :invitation_created_at,
+    :invitation_sent_at,
+    :invitation_accepted_at,
+    :invitation_limit,
+    :invitations_count,
   ].freeze
 
   # FORM_ATTRIBUTES
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = [
+    :invited_by,
     :email,
     :encrypted_password,
     :reset_password_token,
@@ -82,6 +99,13 @@ class UserDashboard < Administrate::BaseDashboard
     :confirmed_at,
     :confirmation_sent_at,
     :unconfirmed_email,
+    :role,
+    :invitation_token,
+    :invitation_created_at,
+    :invitation_sent_at,
+    :invitation_accepted_at,
+    :invitation_limit,
+    :invitations_count,
   ].freeze
 
   # Overwrite this method to customize how users are displayed
